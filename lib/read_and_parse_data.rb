42859ee780b9e559013d962e9ec01183c8cf159d
Maciej Sypniewski <msypniewski511@gmail.com>
2020-09-08 22:43:58 +0100

    Added spec for ReadAndParseDate

--- lib/read_and_parse_data.rb
+++ lib/read_and_parse_data.rb
@@ -3,6 +3,7 @@ module LoggerParser
   class ReadAndParseData
     def initialize(file)
       @file = file
+
     end
 
     def call
@@ -13,8 +14,15 @@ module LoggerParser
     attr_reader :file
 
     def read_and_parse_file
+      file.each_line do |line|
+        tmp = line.split()
+        puts "#{tmp[0]} 1"
+        puts
+        puts "#{tmp[0]} 1"
+        puts "\n"
+      end
       # TODO
-      puts "/some_page/1 1\n\n/some_page/1 1\n\n"
+      # puts "/some_page/1 1\n\n/some_page/1 1\n\n"
     end
   end
 end
---
+++ spec/lib/read_and_parse_data_spec.rb
@@ -0,0 +1,30 @@
+
+
+module LoggerParser
+  RSpec.describe ReadAndParseData do
+    describe '#call' do
+
+      subject(:object) { described_class.new(file_to_read) }
+
+      let(:file_to_read) { File.open(file_path, 'r') }
+
+      context 'when file contain invalid data' do
+        let(:expectation) { ['/some_page/1', '123.456.789.012'] }
+        let(:file_path) { File.expand_path('../fixtures/invalidlog.log', __dir__) }
+
+        it 'does not print anything' do
+          object.call
+          expect(List.count).to be_zero
+        end
+      end
+
+      context 'when file contain valid data' do
+        let(:expectation) { ['/some_page/1', '123.456.789.012'] }
+        let(:file_path) { File.expand_path('../fixtures/log.log', __dir__) }
+
+        it {object.call}
+      end
+      
+    end  
+  end
+end
